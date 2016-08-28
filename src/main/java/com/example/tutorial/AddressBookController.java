36336b30303520a6404ac47857a1e84ad11e7ec9
Your Name <705366435@qq.com>
2016-08-28 16:11:36 +0800

    first commit

--- src/main/java/com/example/tutorial/AddressBookController.java
+++ src/main/java/com/example/tutorial/AddressBookController.java
@@ -1,14 +1,26 @@
 package com.example.tutorial;
 
+import java.time.LocalDateTime;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.taojoe.Transformer;
+import com.github.taojoe.proto.Transform;
+import com.google.protobuf.ByteString;
 
 @RestController
 public class AddressBookController {
     @RequestMapping("person")
     AddressBookProtos.Person person() {
+    	Transformer trans=new Transformer();
+    	AddressBookProtos.Person.Builder user= AddressBookProtos.Person.newBuilder().setUid("uu").addTags("aa").addTags("bb")
+                 .setLoginTime(LocalDateTime.now().toString())
+                 .setLevelType(Transform.UserLevelType.LV0)
+                 .setAvatar(ByteString.copyFrom("abc".getBytes()))
+                 .setEnabled(true);
     	
-    	
+    	Person person=trans.messageToJava(message, clz)
     	
         return AddressBookProtos.Person.newBuilder()
                 .setId(1234)
